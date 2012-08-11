97862c1ff00f6729d6331c3167d4e5fff5892e4f
Paul Visschers <->
2012-08-11 18:25:57 +0200

    Removed thread-safety again

--- src/Network/Channel/Client.hs
+++ src/Network/Channel/Client.hs
@@ -2,44 +2,34 @@ module Network.Channel.Client where
 
 import System.IO
 import Network
-import Control.Concurrent.MVar
 import Control.Applicative
 
-data Channel a b = Channel (MVar Handle)
+data Channel a b = Channel Handle
 
 connect :: HostName -> PortNumber -> IO (Channel a b)
 connect hostName portNumber = do
-  handle <- connectTo hostName (PortNumber portNumber)
-  hSetBuffering handle LineBuffering
-  hvar <- newMVar handle
-  return (Channel hvar)
+  h <- connectTo hostName (PortNumber portNumber)
+  hSetBuffering h LineBuffering
+  return (Channel h)
 
 close :: Channel a b -> IO ()
-close = withChannel hClose
+close (Channel h) = hClose h
 
 send :: Show b => b -> Channel a b -> IO ()
-send x = withChannel $ \h -> hPutStr h (show x ++ "\n")
+send x (Channel h) = hPutStr h (show x ++ "\n")
 
 receive :: Read a => Channel a b -> IO a
-receive ch = do
-  mx <- tryReceive ch
-  case mx of
-    Nothing -> receive ch
-    Just x -> return x
+receive (Channel h) = read <$> hGetLine h
 
 isEmpty :: Channel a b -> IO Bool
-isEmpty = withChannel $ \h -> not <$> hReady h
+isEmpty (Channel h) = not <$> hReady h
 
-tryReceive :: Read a => Channel a b -> IO (Maybe a)
-tryReceive = withChannel $ \h -> do
-  b <- hReady h
-  if b
-    then Just . read <$> hGetLine h
-    else return Nothing
-  
-withChannel :: (Handle -> IO c) -> Channel a b -> IO c
-withChannel f (Channel hvar) = do
-  h <- takeMVar hvar
-  x <- f h
-  putMVar hvar h
-  return x
+receiveAll :: Read a => Channel a b -> IO [a]
+receiveAll ch = do
+  b <- isEmpty ch
+  if b then do
+    return []
+  else do
+    x <- receive ch
+    xs <- receiveAll ch
+    return (x : xs)
